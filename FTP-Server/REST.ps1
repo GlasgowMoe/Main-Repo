4ec750b8ef693198faa48aef82d4d4a02eb06f63
Moe Kandi <moe.kandi@velocitycloud.com>
2019-11-11 15:19:04 +0000

    delete FTP-Server

--- FTP-Server/REST.ps1
+++
@@ -1,9 +0,0 @@
- #This part allows us to connect to Vcamp API.
- function Token {
-    $bearerAuthValue = "Bearer Jb9LmZtRuWdsEpmyRTMt"
-    $headers = @{ Authorization = $bearerAuthValue }
-    $Invoke = invoke-WebRequest -uri 'https://velocity.vcamp.io/api/data_centers/BNA4/servers?all=true&hostname=c0zm8898&running=true' -Headers $headers
-    $Invoke
-   }
-
-
